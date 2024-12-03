234003d4c626fb6e5fc4d280c310e7addd23a98c
shaaloom <52926746+shaaloom@users.noreply.github.com>
2024-12-03 18:40:31 +0000

    Add files via upload

---
+++ bibliotheque (1).sql
@@ -0,0 +1,118 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le : lun. 02 déc. 2024 à 14:45
+-- Version du serveur : 8.3.0
+-- Version de PHP : 8.2.18
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `bibliotheque`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `commentaires`
+--
+
+DROP TABLE IF EXISTS `commentaires`;
+CREATE TABLE IF NOT EXISTS `commentaires` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `contenu` text NOT NULL,
+  `livre_id` int DEFAULT NULL,
+  `utilisateur_id` int DEFAULT NULL,
+  `date_commentaire` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `livre_id` (`livre_id`),
+  KEY `utilisateur_id` (`utilisateur_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `historique_suppressions`
+--
+
+DROP TABLE IF EXISTS `historique_suppressions`;
+CREATE TABLE IF NOT EXISTS `historique_suppressions` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `titre` varchar(255) DEFAULT NULL,
+  `auteur` varchar(100) DEFAULT NULL,
+  `date_suppression` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `livres`
+--
+
+DROP TABLE IF EXISTS `livres`;
+CREATE TABLE IF NOT EXISTS `livres` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `titre` varchar(255) NOT NULL,
+  `auteur` varchar(100) NOT NULL,
+  `date_publication` date DEFAULT NULL,
+  `utilisateur_id` int DEFAULT NULL,
+  `date_ajout` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `utilisateur_id` (`utilisateur_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Déclencheurs `livres`
+--
+DROP TRIGGER IF EXISTS `after_livre_delete`;
+DELIMITER $$
+CREATE TRIGGER `after_livre_delete` AFTER DELETE ON `livres` FOR EACH ROW BEGIN
+    INSERT INTO Historique_Suppressions (titre, auteur)
+    VALUES (OLD.titre, OLD.auteur);
+END
+$$
+DELIMITER ;
+DROP TRIGGER IF EXISTS `before_livre_insert`;
+DELIMITER $$
+CREATE TRIGGER `before_livre_insert` BEFORE INSERT ON `livres` FOR EACH ROW BEGIN
+    DECLARE msg VARCHAR(255);
+    IF NEW.utilisateur_id IS NULL THEN
+        SET msg = 'Erreur : Un livre doit être associé à un utilisateur.';
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = msg;
+    END IF;
+END
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `utilisateurs`
+--
+
+DROP TABLE IF EXISTS `utilisateurs`;
+CREATE TABLE IF NOT EXISTS `utilisateurs` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nom` varchar(50) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `role` enum('admin','utilisateur') DEFAULT 'utilisateur',
+  `date_creation` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
